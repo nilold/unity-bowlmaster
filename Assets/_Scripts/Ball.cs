1dc373f4742fdf13f043104a70ecf25b392de0b8
nilo <nilold@gmail.com>
2018-09-17 23:03:37 -0300

    prefabed pins

---
+++ Assets/_Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private Rigidbody ballBody;
+    private AudioSource audioSource;
+
+    [SerializeField] Vector3 launchVelocity;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        ballBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        Launch();
+    }
+
+    public void Launch()
+    {
+        ballBody.velocity = launchVelocity;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+
+    [SerializeField] Ball ball;
+    [SerializeField] Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		//offset = new Vector3(0f, 20f, -50f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(transform.position.z < 1800){
+            transform.position = ball.transform.position + offset;
+        }
+	}
+}
