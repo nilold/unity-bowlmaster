dd27a838d1c44afc872c00a9eea497645e69f16d
nilo <nilold@gmail.com>
2018-09-23 14:47:03 -0300

    started score logic and TDD

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+[TestFixture]
+public class ActionMasterTest
+{
+
+    private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+
+
+    [Test]
+    public void T00ScoreMasterTestsSimplePasses()
+    {
+        Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void T01OneStrikeReturnsEndTurn()
+    {
+        ActionMaster actionMaster = new ActionMaster();
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+}
