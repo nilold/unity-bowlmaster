221eb22110c960e7490b0cd832c06798da7a3859
nilo <nilold@gmail.com>
2018-10-07 19:45:21 -0300

    score algorothim almost working

--- Assets/_Scripts/ScoreMaster.cs
+++ Assets/_Scripts/ScoreMaster.cs
@@ -23,19 +23,76 @@ public class ScoreMaster
     {
         List<int> frameList = new List<int>();
 
-        int index = 1;
         int frameScore = 0;
+        int bonusScore = 0;
+
+        bool isStriked = false;
+        bool handleStrike = false;
+        bool isSpared = false;
+        int strikeBonusCount = 0;
+
+        int index = 0;
+
         foreach(int roll in rolls){
-            if(index%2 == 1){ //first row of frame
+            //handle bonuses
+            if (isStriked)
+            {
+                bonusScore += roll;
+                strikeBonusCount++;
+                if (strikeBonusCount == 2)
+                {
+                    handleStrike = true;
+                }
+            }
+
+            if (isSpared){
+                bonusScore += roll;
+                isSpared = false;
+                frameScore += bonusScore;
+                frameList.Add(frameScore);
+                bonusScore = 0;
+                frameScore = 0;
+            }
+
+
+
+            if(index%2 == 0){ //first row of frame
                 if(roll == 10){
                     //strike
-
-                } else{
+                    isStriked = true;
                     frameScore = roll;
+                    //index++;
+                    //frameList.Add(frameScore);
+                } else{
+                    frameScore += roll;
                 }
             } else{
                 //second roll of frame
+                if(rolls[index - 1] + rolls[index] == 10){
+                    //spare
+                    isSpared = true;
+                    frameScore += roll;
+                }else{
+                    //normal end of frame
+                    frameScore += roll;
+                    if(!isStriked){
+                        frameList.Add(frameScore);
+                        frameScore = 0;
+                    }
+                }
             }
+
+            if(handleStrike){
+                handleStrike = false;
+                isStriked = false;
+                frameList.Add(frameScore);
+                frameList.Add(bonusScore);
+                frameScore = 0;
+                bonusScore = 0;
+                strikeBonusCount = 0;
+            }
+
+            index++;
         }
 
         return frameList;
